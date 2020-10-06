f974434cf5d7a7450cf10eba1a36534e534bfa03
Maxwell Kang <code.athei@gmail.com>
2020-10-06 16:49:15 -0700

    Update Unity examples

---
+++ UnityProject/Assets/Examples/001 - Define Sheet Containers/ItemSheet.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Cathei.BakingSheet.Examples
+{
+    public class ItemSheet : Sheet<ItemSheet.Row>
+    {
+        public class Row : SheetRow
+        {
+            public string Name { get; private set; }
+            public int Price { get; private set; }
+        }
+    }
+}
---
+++ UnityProject/Assets/Examples/002 - Import From Excel/Editor/ExcelPostprocessor.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using UnityEditor;
+using UnityEngine;
+
+namespace Cathei.BakingSheet.Examples
+{
+    public class ExcelPostprocessor : AssetPostprocessor
+    {
+        static async void OnPostprocessAllAssets(string[] importedAssets, string[] deletedAssets, string[] movedAssets, string[] movedFromAssetPaths)
+        {
+            // automatically run postprocessor if any excel file is imported
+            string excelAsset = importedAssets.FirstOrDefault(x => x.EndsWith(".xlsx"));
+
+            if (excelAsset != null)
+            {
+                var excelPath = Path.GetDirectoryName(excelAsset);
+
+                var logger = new UnityLogger();
+                var sheetContainer = new SheetContainer(logger);
+
+                // create excel importer from path
+                var excelImporter = new ExcelSheetImporter(excelPath);
+
+                // bake sheets from excel importer
+                await sheetContainer.Bake(excelImporter);
+
+                // save datasheet to streaming assets
+                await sheetContainer.Store(Path.Combine(Application.streamingAssetsPath, "Excel"));
+
+                AssetDatabase.Refresh();
+
+                Debug.Log("Excel sheet converted.");
+            }
+        }
+    }
+}
