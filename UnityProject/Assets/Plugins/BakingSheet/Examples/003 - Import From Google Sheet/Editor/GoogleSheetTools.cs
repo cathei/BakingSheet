68213d5dcf5ea973a59425e855ae8c0f0d6bc2cc
Maxwell Kang <code.athei@gmail.com>
2020-10-06 16:59:32 -0700

    Update Unity examples

---
+++ UnityProject/Assets/Plugins/BakingSheet/Examples/003 - Import From Google Sheet/Editor/GoogleSheetTools.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Cathei.BakingSheet.Examples
+{
+    public static class GoogleSheetTools
+    {
+
+    }
+
+}
---
+++ UnityProject/Assets/Plugins/BakingSheet/Examples/004 - Runtime Usage/SheetLoader.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+namespace Cathei.BakingSheet.Examples
+{
+    public class SheetLoader : MonoBehaviour
+    {
+        public SheetContainer Sheet { get; private set; }
+
+        private void Start()
+        {
+            Sheet = new SheetContainer(new UnityLogger());
+            Sheet.Load(Path.Combine(Application.streamingAssetsPath, "Excel"));
+        }
+
+    }
+
+}
