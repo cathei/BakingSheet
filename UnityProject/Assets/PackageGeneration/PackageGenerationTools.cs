ef0fe21a0e94f864093d2b9f5c48932b9bea28f5
Maxwell Kang <code.athei@gmail.com>
2020-10-06 18:05:23 -0700

    UnityPackage generation tool

---
+++ UnityProject/Assets/PackageGeneration/PackageGenerationTools.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+namespace Cathei.BakingSheet.Editor
+{
+    public static class PackageGenerationTools
+    {
+        [MenuItem("Tools/Generate Package")]
+        public static void GeneratePackage()
+        {
+            var version = Environment.GetEnvironmentVariable("PROJECT_VERSION");
+
+            if (version != null)
+                PlayerSettings.bundleVersion = version;
+
+            AssetDatabase.ExportPackage(new string [] {
+                "Assets/Plugins/BakingSheet"
+            }, $"BakingSheet.{PlayerSettings.bundleVersion}.unitypackage", ExportPackageOptions.Recurse);
+
+            Debug.Log("Generating Unity Package Completed");
+        }
+    }
+}
