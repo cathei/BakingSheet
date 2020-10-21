ed0cb70674c7d5ec1da3e03e0231bedf06b5b6fe
Maxwell Kang <code.athei@gmail.com>
2020-10-20 22:42:27 -0700

    use NReco.Csv

--- BakingSheet.Importers.Csv/CsvSheetImporter.cs
+++ BakingSheet.Importers.Csv/CsvSheetImporter.cs
@@ -1,6 +1,9 @@
 ﻿using System.Collections.Generic;
+using System.Data;
+using System.Globalization;
 using System.IO;
 using System.Threading.Tasks;
+using NReco.Csv;
 
 namespace Cathei.BakingSheet
 {
@@ -8,13 +11,17 @@ namespace Cathei.BakingSheet
     {
         private string _loadPath;
         private string _searchPattern;
-
-        private class CsvTable : List<IList<string>>
-        {
-            public CsvTable(IEnumerable<IList<string>> collection) : base(collection) { }
-        }
-
         private Dictionary<string, CsvTable> _dataTables;
+
+        private class CsvTable : List<List<string>>
+        {
+            public List<string> AddRow()
+            {
+                var row = new List<string>();
+                Add(row);
+                return row;
+            }
+        }
 
         public CsvSheetImporter(string loadPath, string searchPattern = "*.csv")
         {
@@ -54,9 +61,17 @@ namespace Cathei.BakingSheet
                 using (var stream = File.Open(file, FileMode.Open, FileAccess.Read, FileShare.ReadWrite))
                 using (var reader = new StreamReader(stream))
                 {
-                    var text = reader.ReadToEndAsync();
-                    var data = new CsvTable(CsvParser.Parse(reader, ',', '"'));
-                    _dataTables[Path.GetFileNameWithoutExtension(file)] = data;
+                    var csv = new CsvReader(reader);
+                    var table = new CsvTable();
+
+                    while (csv.Read())
+                    {
+                        var row = table.AddRow();
+                        for (int i = 0; i < csv.FieldsCount; ++i)
+                            row.Add(csv[i]);
+                    }
+
+                    _dataTables[Path.GetFileNameWithoutExtension(file)] = table;
                 }
             }
 
---
+++ UnityProject/Assets/Plugins/BakingSheet/Examples/004 - Import From CSV/Editor/CsvPostprocessor.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using UnityEditor;
+using UnityEngine;
+
+namespace Cathei.BakingSheet.Examples
+{
+    public class CsvPostprocessor : AssetPostprocessor
+    {
+        static async void OnPostprocessAllAssets(string[] importedAssets, string[] deletedAssets, string[] movedAssets, string[] movedFromAssetPaths)
+        {
+            // automatically run postprocessor if any excel file is imported
+            string csvAsset = importedAssets.FirstOrDefault(x => x.EndsWith(".csv"));
+
+            if (csvAsset != null)
+            {
+                var csvPath = Path.GetDirectoryName(csvAsset);
+
+                var logger = new UnityLogger();
+                var sheetContainer = new SheetContainer(logger);
+
+                // create excel importer from path
+                var csvImporter = new CsvSheetImporter(csvPath);
+
+                // bake sheets from excel importer
+                await sheetContainer.Bake(csvImporter);
+
+                // save datasheet to streaming assets
+                await sheetContainer.Store(Path.Combine(Application.streamingAssetsPath, "CSV"));
+
+                AssetDatabase.Refresh();
+
+                Debug.Log("CSV sheet converted.");
+            }
+        }
+    }
+}
