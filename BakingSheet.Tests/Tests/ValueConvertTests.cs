4df0c472f0b24cfefa2f5372cee2cfe27d560057
Maxwell Kang <code.athei@gmail.com>
2021-07-26 15:08:36 -0700

    update test case

--- BakingSheet.Tests/Tests/ValueConvertTests.cs
+++ BakingSheet.Tests/Tests/ValueConvertTests.cs
@@ -11,12 +11,10 @@ namespace Cathei.BakingSheet.Tests
     public class ValueConvertTests : IDisposable
     {
         private TestFileSystem _fileSystem;
-        private CsvSheetConverter _converter;
 
         public ValueConvertTests()
         {
             _fileSystem = new TestFileSystem();
-            _converter = new CsvSheetConverter("csvdata", TimeZoneInfo.Utc, fileSystem: _fileSystem);
         }
 
         public void Dispose()
@@ -29,8 +27,6 @@ namespace Cathei.BakingSheet.Tests
             yield return new object[] { typeof(int), "3", 3 };
             yield return new object[] { typeof(float), "3.14", 3.14f };
             yield return new object[] { typeof(string), "abcd", "abcd" };
-            yield return new object[] { typeof(DateTime), "2020-01-01", new DateTime(2020, 01, 01) };
-            yield return new object[] { typeof(DateTime), "8/22/1994 10:34:12 PM", new DateTime(1994, 8, 22, 22, 34, 12) };
             yield return new object[] { typeof(TimeSpan), "20:00", new TimeSpan(20, 0, 0) };
             yield return new object[] { typeof(TimeSpan), "12:34:56", new TimeSpan(12, 34, 56) };
         }
@@ -39,7 +35,8 @@ namespace Cathei.BakingSheet.Tests
         [MemberData(nameof(GetStringToValueTestData))]
         public void TestStringToValue(Type type, string data, object expected)
         {
-            var value = _converter.StringToValue(type, data);
+            var converter = new CsvSheetConverter("csvdata", TimeZoneInfo.Utc, fileSystem: _fileSystem);
+            var value = converter.StringToValue(type, data);
 
             Assert.Equal(expected, value);
         }
@@ -49,8 +46,6 @@ namespace Cathei.BakingSheet.Tests
             yield return new object[] { typeof(int), 3, "3" };
             yield return new object[] { typeof(float), 3.14f, "3.14" };
             yield return new object[] { typeof(string), "abcd", "abcd" };
-            yield return new object[] { typeof(DateTime), new DateTime(2020, 01, 01), "1/1/2020 12:00:00 AM" };
-            yield return new object[] { typeof(DateTime), new DateTime(1994, 8, 22, 22, 34, 12), "8/22/1994 10:34:12 PM" };
             yield return new object[] { typeof(TimeSpan), new TimeSpan(20, 0, 0), "20:00:00" };
             yield return new object[] { typeof(TimeSpan), new TimeSpan(12, 34, 56), "12:34:56" };
         }
@@ -59,7 +54,40 @@ namespace Cathei.BakingSheet.Tests
         [MemberData(nameof(GetValueToStringTestData))]
         public void TestValueToString(Type type, object value, string expected)
         {
-            var data = _converter.ValueToString(type, value);
+            var converter = new CsvSheetConverter("csvdata", TimeZoneInfo.Utc, fileSystem: _fileSystem);
+            var data = converter.ValueToString(type, value);
+
+            Assert.Equal(expected, data);
+        }
+
+        public static IEnumerable<object[]> GetStringToDateTimeTestData()
+        {
+            yield return new object[] { typeof(DateTime), "2020-01-01", new DateTime(2020, 01, 01), TimeZoneInfo.Utc };
+            yield return new object[] { typeof(DateTime), "8/22/1994 10:34:12 PM", new DateTime(1994, 8, 22, 22, 34, 12), TimeZoneInfo.Utc };
+        }
+
+        [Theory]
+        [MemberData(nameof(GetStringToDateTimeTestData))]
+        public void TestStringToDateTime(Type type, string data, object expected, TimeZoneInfo timeZoneInfo)
+        {
+            var converter = new CsvSheetConverter("csvdata", timeZoneInfo, fileSystem: _fileSystem);
+            var value = converter.StringToValue(type, data);
+
+            Assert.Equal(expected, value);
+        }
+
+        public static IEnumerable<object[]> GetDateTimeToStringTestData()
+        {
+            yield return new object[] { typeof(DateTime), new DateTime(2020, 01, 01), "1/1/2020 12:00:00 AM", TimeZoneInfo.Utc };
+            yield return new object[] { typeof(DateTime), new DateTime(1994, 8, 22, 22, 34, 12), "8/22/1994 10:34:12 PM", TimeZoneInfo.Utc };
+        }
+
+        [Theory]
+        [MemberData(nameof(GetDateTimeToStringTestData))]
+        public void TestDateTimeToString(Type type, object value, string expected, TimeZoneInfo timeZoneInfo)
+        {
+            var converter = new CsvSheetConverter("csvdata", timeZoneInfo, fileSystem: _fileSystem);
+            var data = converter.ValueToString(type, value);
 
             Assert.Equal(expected, data);
         }
