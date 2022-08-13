b890ceaa394c7de02b6a57dd5b30ce90e5be8d7b
Maxwell Kang <code.athei@gmail.com>
2022-08-12 19:44:38 -0700

    add asset store tools package

---
+++ UnityProject/Packages/com.unity.asset-store-tools/Editor/AssetStoreUploader/Scripts/ASAnalytics.cs
@@ -0,0 +1,46 @@
+﻿using UnityEditor;
+using UnityEngine.Analytics;
+
+namespace AssetStoreTools.Uploader
+{
+    public static class ASAnalytics
+    {
+        private const int VersionId = 2;
+        private const int MaxEventsPerHour = 20;
+        private const int MaxNumberOfElements = 1000;
+
+        private const string VendorKey = "unity.assetStoreTools";
+        private const string EventName = "assetStoreTools";
+        
+        static bool EnableAnalytics()
+        {
+            var result = EditorAnalytics.RegisterEventWithLimit(EventName, MaxEventsPerHour, MaxNumberOfElements, VendorKey, VersionId);
+            return result == AnalyticsResult.Ok;
+        }
+
+        [System.Serializable]
+        public struct AnalyticsData
+        {
+            public string ToolVersion;
+            public string PackageId;
+            public string Category;
+            public bool UsedValidator;
+            public string ValidatorResults;
+            public string UploadFinishedReason;
+            public double TimeTaken;
+            public int PackageSize;
+            public string Workflow;
+        }
+
+        public static void SendUploadingEvent(AnalyticsData data)
+        {
+            if (!EditorAnalytics.enabled)
+                return;
+
+            if (!EnableAnalytics())
+                return;
+
+            EditorAnalytics.SendEventWithLimit(EventName, data, VersionId);
+        }
+    }
+}
---
+++ UnityProject/Packages/com.unity.asset-store-tools/Editor/AssetStoreUploader/Scripts/UploadWindow/AllPackageView/PackageViewStorer.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+
+namespace AssetStoreTools.Uploader
+{
+    public static class PackageViewStorer
+    {
+        private static readonly Dictionary<string, PackageView> SavedPackages = new Dictionary<string, PackageView>();
+        
+        internal static PackageView GetPackage(PackageData packageData)
+        {
+            string versionId = packageData.VersionId;
+            if (SavedPackages.ContainsKey(versionId))
+            {
+                var savedPackage = SavedPackages[versionId];
+                savedPackage.UpdateDataValues(packageData);
+                return savedPackage;
+            }
+
+            var package = new PackageView(packageData);
+            SavedPackages.Add(package.VersionId, package);
+            return package;
+        }
+
+        public static void Reset()
+        {
+            SavedPackages.Clear();
+        }
+    }
+}
