721adcc79ade10276dcf066058b5f64d1d6f404e
Maxwell Kang <code.athei@gmail.com>
2022-10-05 01:09:44 -0700

    update so import sample

--- UnityProject/Assets/Samples/007 - Using ScriptableObject/Runtime/SampleImportScriptableObject.cs
+++ UnityProject/Assets/Samples/007 - Using ScriptableObject/Runtime/SampleImportScriptableObject.cs
@@ -3,14 +3,30 @@ using UnityEngine;
 
 namespace Cathei.BakingSheet.Examples
 {
-    public class SampleImportScriptableRow : MonoBehaviour
+    public class SampleImportScriptableObject : MonoBehaviour
     {
-        public SheetContainerScriptableObject container;
+        private SheetContainer container;
+        public SheetContainerScriptableObject containerSO;
 
-        public void Awake()
+        public async void Start()
         {
+            var importer = new ScriptableObjectSheetImporter(containerSO);
 
+            container = new SheetContainer();
+            await container.Bake(importer);
 
+            foreach (var dungeon in container.Dungeons)
+            {
+                Debug.Log(dungeon.Name);
+                Debug.Log(dungeon.Monsters[0].Ref.Name);
+                Debug.Log(dungeon.Items[0].Ref.Name);
+            }
+
+            foreach (var asset in container.Assets)
+            {
+                Debug.Log(asset.Direct.Get<Object>());
+                Debug.Log(asset.Resource.Load<Object>());
+            }
         }
     }
 }
